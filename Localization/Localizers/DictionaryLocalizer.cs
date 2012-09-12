bc3b1d51940ba36df6c8ea2c6bb49612ef127c06
jbeijar <jbeijar@mios.fi>
2012-09-12 15:35:45 +0300

    Allow specifying a default localizer.

--- Localization/Localizers/DictionaryLocalizer.cs
+++ Localization/Localizers/DictionaryLocalizer.cs
@@ -4,13 +4,21 @@ using System.Collections.Generic;
 namespace Mios.Localization.Localizers {
 	public class DictionaryLocalizer {
 		private readonly IDictionary<string, string> source;
-		public DictionaryLocalizer(IDictionary<string,string> source) {
-			this.source = source;
-		}
-		public LocalizedString Localize(string original, params object[] args) {
+	  private readonly Localizer defaultLocalizer;
+
+	  public DictionaryLocalizer(IDictionary<string, string> source) : this(source, NullLocalizer.Instance) {
+    }
+	  public DictionaryLocalizer(IDictionary<string,string> source, Localizer defaultLocalizer) {
+	    this.source = source;
+	    this.defaultLocalizer = defaultLocalizer;
+	  }
+
+	  public LocalizedString Localize(string original, params object[] args) {
 			if(original==null) return new LocalizedString(); 
 			string localized;
-			source.TryGetValue(original, out localized);
+      if(!source.TryGetValue(original, out localized)) {
+        return defaultLocalizer(original, args);
+      }
 			if(localized!=null) {
 				localized = String.Format(localized, args);
 			}
