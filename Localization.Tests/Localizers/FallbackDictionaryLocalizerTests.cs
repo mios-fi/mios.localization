0e8c47bb6ce300e64506fa35723afb8475615423
jbeijar <jbeijar@mios.fi>
2012-10-12 11:16:23 +0300

    Rename source file to match class

--- Localization.Tests/Localizers/FallbackDictionaryLocalizerTests.cs
+++ Localization.Tests/Localizers/FallbackDictionaryLocalizerTests.cs
@@ -1,8 +1,4 @@
 ﻿using Mios.Localization.Localizers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Xunit;
 
 namespace Mios.Localization.Tests.Localizers {
@@ -13,17 +9,23 @@ namespace Mios.Localization.Tests.Localizers {
 				{"sv-se","k","Key"}
 			};
 			var t = new FallbackDictionaryLocalizer(dictionary, "sv-se");
-			Assert.Equal("Key", t.Localize("k", "sv-se"));
+			Assert.Equal("Key", t.Localize("k"));
 		}
-		[Fact]
-		public void ShouldReturnValueFromDefaultLocalizerIfNotFound() {
-			var dictionary = new LocalizationDictionary();
-			var t = new FallbackDictionaryLocalizer(dictionary, "sv-se") { 
-				DefaultLocalizer = (k,p) => new LocalizedString(k,"default")
-			};
-			Assert.Equal("default", t.Localize("k", "sv-se"));
-		}
-		[Fact]
+    [Fact]
+    public void ShouldReturnValueFromDefaultLocalizerIfNotFound() {
+      var dictionary = new LocalizationDictionary();
+      var t = new FallbackDictionaryLocalizer(dictionary, "sv-se") {
+        DefaultLocalizer = (k, p) => new LocalizedString(k, "default")
+      };
+      Assert.Equal("default", t.Localize("k"));
+    }
+    [Fact]
+    public void ShouldReturnEmptyLocalizedStringIfKeyIsNull() {
+      var dictionary = new LocalizationDictionary();
+      var t = new FallbackDictionaryLocalizer(dictionary, "sv-se");
+      Assert.Null(t.Localize(null).Localization);
+    }
+    [Fact]
 		public void ShouldReturnFallbackLocalizationIfFullLocaleNotFound() {
 			var dictionary = new LocalizationDictionary { 
 				{"sv","k","Fallback"}
